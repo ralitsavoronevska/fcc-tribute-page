703c1ac8ac1a33470ba1bf7cd3138d64b9b8301d
ralitsavoronevska <ralicavoronevska@gmail.com>
2025-07-26 23:13:33 +0300

    Updates

--- js/btn-script.js
+++ js/btn-script.js
@@ -1,8 +1,7 @@
-(function (){
-  "use strict";
-  let scroll = document.querySelector('.scrollTop');
+window.addEventListener("DOMContentLoaded", () => {
+  let scroll = document.querySelector('.scrollToTop');
   window.addEventListener('scroll', function() {
     scroll.classList.toggle("active", window.scrollY > 500);
   });
   scroll.onclick = function() { window.scrollTo({ top: 0, behavior: 'smooth' }); }
-}());
+});
