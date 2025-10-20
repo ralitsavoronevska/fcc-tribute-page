73468be39dc2d4f442d4572a69af211f8c80593c
ralitsavoronevska <ralicavoronevska@gmail.com>
2025-10-20 23:56:01 +0300

    Initial commit

---
+++ dist/js/btn-script-min.js
@@ -0,0 +1,1 @@
+window.addEventListener("DOMContentLoaded",()=>{let scroll=document.querySelector('.scrollToTop');window.addEventListener('scroll',function(){scroll.classList.toggle("active",window.scrollY>500)});scroll.onclick=function(){window.scrollTo({top:0,behavior:'smooth'})}})
